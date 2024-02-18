1982efd804022745d6cd7cf64c9d4ea33e8e7e44
julius-kazibwe <kazibwej54@gmail.com>
2024-02-18 11:26:23 +0300

    Julio

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,24 @@
 from django.urls import path
-from . import views
+from .views import (
+    remove_from_cart,
+    reduce_quantity_item,
+    add_to_cart,
+    ProductView,
+    HomeView,
+    OrderSummaryView,
+    CheckoutView,
+    PaymentView
+)
 
+app_name ='products'
 
 urlpatterns = [
-    #path('', views.home, name='home'),
-    path('', views.index, name=''),
-    path('<int:product_id>/', views.description, name='description'),
+    path('', HomeView.as_view(), name='home'),
+    path('product/<pk>/', ProductView.as_view(), name='product'),
+    path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
+    path('add-to-cart/<pk>/', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<pk>/', remove_from_cart, name='remove-from-cart'),
+    path('reduce-quantity-item/<pk>/', reduce_quantity_item, name='reduce-quantity-item')
 ]
